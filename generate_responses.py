12ddf6b0c5442ca3e294793e211cd8bea0084064
Sam Chow <sachow@opendns.com>
2016-11-06 23:02:53 -0800

    finished implementing responses method

--- generate_responses.py
+++ generate_responses.py
@@ -1,5 +1,7 @@
 import requests
+import bs4
 from bs4 import BeautifulSoup
+import pymongo
 
 # CONSTANTS
 BASE_URL = 'http://overwatch.gamepedia.com'
@@ -19,20 +21,39 @@ def main():
     url_stubs = get_hero_response_links()
     # Map the urls to the base, and write them each to their own file
     urls = map(lambda u: BASE_URL + u, url_stubs)
-    generate_pickles(urls)
+    generate_all_responses(urls)
 
-def generate_pickles(urls):
+def generate_all_responses(urls):
+    responses = {}
     for link in urls:
         # generate soup
         r = requests.get(link)
         soup = BeautifulSoup(r.text, 'html.parser')
 
         # parse soup for audio files
-        responses = []
-        for element in soup.find_all('audio'):
-            responses.append(element.get('src'))
+        # since we need the hero name, the line, and the link
+        # this is a dict of strings that leads to a tuple of (character, href)
 
-
+        # we need to restrict it to finding the table:
+        table = soup.find('table')
+        # since it's the only table on page, just get it at 0th index
+        for element in table.children:
+            # we need to get the child tds that have the lines, which are the last two:
+            if type(element) == bs4.element.Tag:
+                for tr in element.children:
+                    if type(tr) == bs4.element.Tag:
+                        if tr.find_all('audio'):
+                            src = tr.find('audio').get('src')
+                            # insert into the array
+                            # key is lastNode, character is parsed out from the link,
+                            # and the href is simply the src
+                            name = src.split('/')[-1].split('-')[0]
+                            name = name.replace('_', '').replace('\n', '')
+                            charRef = (name, src)
+                            responses[lastNode] = charRef
+                        else:
+                            # we keep track of the last one, next one might be audio
+                            lastNode = tr.text.replace('\n', '')
 
 if __name__ == '__main__':
     main()
